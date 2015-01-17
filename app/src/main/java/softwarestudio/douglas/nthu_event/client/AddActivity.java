9ebbd7ccd34530d4dda3725c4fc29d361a90e2f4
Douglas Wu <dgrey1116@gmail.com>
2015-01-17 16:48:08 +0800

    AddActivity 新增時間選擇功能

--- app/src/main/java/softwarestudio/douglas/nthu_event/client/AddActivity.java
+++ app/src/main/java/softwarestudio/douglas/nthu_event/client/AddActivity.java
@@ -1,32 +1,47 @@
 package softwarestudio.douglas.nthu_event.client;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
+import java.util.List;
 
 
 public class AddActivity extends Activity {
     private EditText eventNameEdt;
     private EditText eventDateEdt;
     private EditText eventPlaceEdt;
+    private EditText eventTimeEdt;
     private EditText eventContentEdt;
     private Button submitBtn;
+    private Button chooseDateBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
         eventNameEdt = (EditText) findViewById(R.id.name_txt);
         eventDateEdt = (EditText) findViewById(R.id.date_txt);
+        eventTimeEdt = (EditText) findViewById(R.id.time_txt);
         eventPlaceEdt = (EditText) findViewById(R.id.location_txt);
         eventContentEdt = (EditText) findViewById(R.id.content_txt);
         submitBtn = (Button) findViewById(R.id.submitEventBtn);
+       // chooseDateBtn = (Button) findViewById(R.id.btn_pickDate);
 
 
         Spinner spinner1 = (Spinner) findViewById(R.id.eventCat1);
@@ -43,9 +58,65 @@ public class AddActivity extends Activity {
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(adapter2);
         spinner2.setOnItemSelectedListener(spnListener2);
+
+        //chooseDateBtn.setOnClickListener(pickDateListener);
+        eventDateEdt.setOnClickListener(pickDateListener);
+        eventTimeEdt.setOnClickListener(pickTimeListener);
+
     }
 
+    /**時間選擇，確認、取消按鈕還要再加*/
+    private View.OnClickListener pickTimeListener
+            = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            // TODO Auto-generated method stub
+            Calendar mcurrentTime = Calendar.getInstance();
+            int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+            int minute = mcurrentTime.get(Calendar.MINUTE);
+            TimePickerDialog mTimePicker;
 
+            mTimePicker = new TimePickerDialog(AddActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                    eventTimeEdt.setText( selectedHour + ":" + selectedMinute);
+                }
+            }, hour, minute, false);//Yes 24 hour time
+            mTimePicker.setTitle("選擇時間");
+            mTimePicker.show();
+        }
+    };
+
+    private View.OnClickListener pickDateListener
+            = new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+            final DatePicker datePicker = new DatePicker(AddActivity.this);
+
+            new AlertDialog.Builder(AddActivity.this)
+                    .setTitle("選擇活動日期")
+                    .setView(datePicker)
+                    .setPositiveButton("確定" ,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    int year = datePicker.getYear();
+                                    int month = datePicker.getMonth();
+                                    int dateOfMonth = datePicker.getDayOfMonth();
+
+                                    String dateStr = year + "/"
+                                            + formatDigit(month + 1) + "/"
+                                            + formatDigit(dateOfMonth);
+                                    eventDateEdt.setText(dateStr);
+                                }
+
+                            }
+
+                    )
+                    .setNegativeButton("取消", null)
+                    .show();
+        }
+    };
     private OnItemSelectedListener spnListener1 =
             new OnItemSelectedListener(){
                 @Override
@@ -72,4 +143,7 @@ public class AddActivity extends Activity {
                 }
 
             };
+    private static String formatDigit(int number) {
+        return number <= 9 ? "0" + number : String.valueOf(number);
+    }
 }
